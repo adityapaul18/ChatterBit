57e9078bf94c36e849e4dae4895004b10494553f
Aditya Paul <aditya.paul18@gmail.com>
2020-09-29 19:02:55 +0530

    day1 project started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>lets get started with whatsapp clone</h1>
+    </div>
+  );
+}
+
+export default App;
