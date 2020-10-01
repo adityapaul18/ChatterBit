d98922e7f7a75002b86ff4a76f526a3ea515de1a
Aditya Paul <62583380+adityapaul18@users.noreply.github.com>
2020-10-01 18:02:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React , { useState  } from 'react';
 import Sidebar from './Sidebar.js'
+import Chat from './Chat.js'
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 
 function App() {
+
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-
+      { !user ? (
+        <h1>LOGIN PAGE</h1>
+      ) : (
         <div className="app_body">
-          <Sidebar />
-          {/* {chat} */}
+        <Router>
+                <Sidebar />
+          <Switch>
+            <Route path="/rooms/:roomId">
+                <Chat/>
+            </Route>
+            <Route path="/">
+                <Chat/>
+            </Route>
+          </Switch>
+        </Router>
         </div>
+      )}
     </div>
   );
 }
