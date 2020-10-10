ac02488aa66add5683f991d5e0ef7a061666f561
Aditya Paul <62583380+adityapaul18@users.noreply.github.com>
2020-10-10 22:14:53 +0530

    completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,38 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 import {Avatar ,IconButton} from "@material-ui/core";
-import ChatIcon from "@material-ui/icons/Chat";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SidebarChat  from "./SidebarChat"
+import db from './firebase';
+import {useStateValue} from "./StateProvider.js";
 
 
+function Sidebar(id, name, addNewchat) {
 
-function Sidebar() {
+    const [rooms,setRooms] = useState([]);
+    const [{user}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        const unsubscribe = db.collection('rooms').onSnapshot((snapshot) => (
+                setRooms(snapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        data: doc.data(),
+                    }))
+                )
+            ));
+
+            return() => {
+                unsubscribe();
+            }
+                }, []);
+
     return (
         <div className="sidebar">
             <div className="sidebar_header">
-                <Avatar />
+                <Avatar src={user?.photoURL}/>
+                     <h3>Welcome {user.displayName} </h3>
 
                 <div className="sidebar_headerRight">
-                     
-                     <IconButton>
-                         <ChatIcon />
-                     </IconButton>
+
                      <IconButton>
                         <MoreVertIcon />
                      </IconButton>
                 </div>
             </div>
 
-            <div className="sidebar_search">
-                searchbar
+            <div className={`sidebar_search ${ 0 && "sidebar_search1" }`}>
+               <div className="sidebar_searchconainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or Start new chat"  type="text" />
+                </div>
             </div>
 
             <div className="sidebar_chat">
-                chat section
+                <SidebarChat addNewChat />
+                {rooms.map(room => ( <SidebarChat key= {room.id} id={room.id} name={room.data.name} />
+                ))}
             </div>
             
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
